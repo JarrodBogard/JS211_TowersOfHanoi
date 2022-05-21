3bb38a3325f8ea6c7d3b4476fba70b0735d453f1
Jarrod Bogard <jarrodbogard@gmail.com>
2022-05-21 18:18:30 -0500

    added gui

--- index.js
+++ index.js
@@ -7,20 +7,47 @@
 // * Why are you get a warning in your console? Fix it.
 // * Delete these comment lines!
 
+
+// let topRow = document.querySelector("#top-row")
+// let middleRow = document.querySelector("#middle-row")
+// let bottomRow = document.querySelector("#bottom-row")
+
+// let largeStone = document.querySelector("#four")
+// let mediumStone = document.querySelector("#three")
+// let smallStone = document.querySelector("#two")
+// let extraSmallStone = document.querySelector("#one")
+
+// topRow = []
+// middleRow = []
+// bottomRow = [largeStone, mediumStone, smallStone, extraSmallStone]
+
 let stone = null
 
+// empty array to hold the value of the stone that was picked up
+let startStone = []
+
+// empty array to hold the value of the last stone in the end stack 
+let endStone = []
+
+// variable for top row and middle row
+const topRow = document.getElementById("top-row")
+const middleRow = document.getElementById("middle-row")
 // this function is called when a row is clicked. 
 // Open your inspector tool to see what is being captured and can be used.
 const selectRow = (row) => {
-  const currentRow = row.getAttribute("data-row")
-  
-  console.log(stone, '----selected row----')
-  console.log("Yay, we clicked an item", row)
-  console.log("Here is the stone's id: ", row.id)
-  console.log("Here is the stone's data-size: ", currentRow)
+  // const currentRow = row.getAttribute("data-row")
 
+  // add event listener that counts moves
+
+  // console.log("Yay, we clicked an item", row)
+  // console.log("Here is the stone's id: ", row.id)
+  // console.log("Here is the stone's data-size: ", currentRow)
+  
   if(stone !== null) {
-    return dropStone(row.id)
+    dropStone(row.id)
+    let rowTracker = row.childNodes
+    console.log(rowTracker.length, "tracker")
+    checkWin()
   } else if (!stone) {
     pickUpStone(row.id)
   }
@@ -30,9 +57,15 @@ const selectRow = (row) => {
 // but there might be something wrong with it...
 const pickUpStone = (rowID) => {
   const selectedRow = document.getElementById(rowID);
+  console.log(`The row that was clicked on is the ${rowID}`)
+
   stone = selectedRow.lastElementChild
+  console.log(stone, "pick up stone")
+
   selectedRow.removeChild(selectedRow.lastElementChild);
-  console.log(stone)
+  console.log(`The stone size is ${stone.id}` )
+  startStone.push(stone.id)
+  console.log(startStone, "----startStone----")
 }
 
 // You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
@@ -40,11 +73,89 @@ const pickUpStone = (rowID) => {
 // Something like: if(!stone){pickupStone} else{dropStone}
 
 const dropStone = (rowID) => {
-  console.log("----dropping stone----")
-  document.getElementById(rowID).appendChild(stone)
-  stone = null
+  const selectedRow = document.getElementById(rowID)
+  let lastStone = selectedRow.lastElementChild
+  console.log(lastStone, "----lastStone----")
+  if(lastStone) {
+    endStone.push(lastStone.id)
+    console.log(lastStone.id, "----lastStone id----")
+    console.log(endStone, "----endStone----")
+  } 
   console.log(stone, "----stone dropped----")
+    if(startStone < endStone  || endStone == 0) {
+      console.log("----dropping stone----")
+      document.getElementById(rowID).appendChild(stone)
+      stone = null
+      console.log("legal move")
+      startStone = []
+      endStone = []
+    } else {
+      startStone = startStone
+      endStone.pop()
+      console.log("illegal move")
+      console.log("startStone: ", startStone, "endStone: ", endStone)
+    }
+
 }
 
-// * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
+const checkWin = () => {
+  if(topRow.childNodes.length === 4 || middleRow.childNodes.length === 4) {
+    console.log("you win!!!")
+    setTimeout(function(){
+      window.location.reload();
+   }, 5000);
+    // const reload = window.location.reload()
+    // myTimeout = setTimeout(reload, 5000)
+    return true
+  }
+}
 
+  // if(lastStone === null ) {
+  //   startStone = []
+  // // }
+  // console.log(endStone, "----endStone---")
+  // console.log(startStone, "---startStone---")
+// }
+
+// const isLegal = () => {
+//   if(startStone < endStone || endStone == 0) {
+//     console.log("Legal Move")
+//     // dropStone()
+//     return true
+//   } else {
+//     console.log("Illegal Move")
+//     return false
+//   }
+// }
+
+
+
+// const isLegal = (start, end) => {
+//   const startRow = document.getElementById(start);
+//   startStone = startRow.lastElementChild
+//   console.log(startStone, "----startStone----")
+//   const endRow = document.getElementById(end);
+//   endStone = endRow.lastElementChild
+//   console.log(endStone, "----endStone----")
+//     if(startStone.id > endStone.id || endStone.id.length === 0) {
+//       console.log("----legal move----")
+//       dropStone()
+//     } else {
+//       console.log("Illegal Move")
+//       return false
+//     }
+// }
+
+// legal move logic
+  // - use this keyword to isolate size of picked stone by the data-size
+  // - once we have size of picked stone compare to size of stone in the selected row that picked stone is to be dropped
+  // - if picked stone size is smaller than size of stone in selected row, drop stone
+  // possible solutions: 
+    // 1) set picked stone logic to its own variable
+    // 2) set selected row to drop stone to its own variable if possible
+    // 3) use conditional to compare these variables
+
+
+// this.data-size > this.selectedRow.lastElementChild.data-size??? or this.getAttribute > this.selectedRow.getAttribute("data-size")
+
+// * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
